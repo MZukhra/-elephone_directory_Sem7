1a80312f8a9ec437c8a2c376b9289bf72fd78945
YalushkinAlexey <109788980+YalushkinAlexey@users.noreply.github.com>
2022-10-16 15:34:40 +0300

    Update add_delete.py

--- add_delete.py
+++ add_delete.py
@@ -6,12 +6,12 @@ from input_output import phone_numbers
 
 
 def add_note():
-    s = ''
     surname = input('введите фамилию ')
     lst = [input('введите имя :'), input('введите номер телефона :'), input('описание :')]
     phone_numbers[surname] = lst
-    print(f'{surname} {s.join(phone_numbers[surname])}')
+    print(f'{surname} {' '.join(phone_numbers[surname])}')
     return phone_numbers
+
 def delete_note(phone_numbers):
     surname = input('введите имя для удаления ')
     phone_numbers.pop(surname)
