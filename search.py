0c2d962753914cce4a38a110a8a66ebb4c3f8065
«Zukhra» <sssmailll@mail.ru>
2022-10-16 22:10:32 +0300

    search

---
+++ search.py
@@ -0,0 +1,23 @@
+# Добавление поиска
+#@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@
+#   author Victoria Burakhina
+#@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@
+
+
+from input_output import phone_numbers
+import User_Interface
+
+
+def search():
+
+    surname = input('введите фамилию ')
+    if phone_numbers.keys().__contains__(surname):
+        print(f'{surname} {" ".join(phone_numbers[surname])}')
+    else:
+
+        print('Данных нет в списке')
+
+        print('Выход в общее меню')
+
+        User_Interface.menu()
+
