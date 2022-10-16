5d2cfec580b25eed8e173c30c7f20cdb15f460be
«Zukhra» <sssmailll@mail.ru>
2022-10-16 22:07:49 +0300

    add_delete

--- add_delete.py
+++ add_delete.py
@@ -2,10 +2,23 @@
 #@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@
 #   author Yalushkin Alexey
 #@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@
-import input_output as data_in
+from input_output import phone_numbers
+import logger
+
+
 
 def add_note():
-    pass
+    surname = input('введите фамилию ')
+    if surname in phone_numbers.keys():
+        phone_numbers.pop(surname)
+    lst = [input('введите имя :'), input('введите номер телефона :'), input('описание :')]
+    phone_numbers[surname] = lst
+    print(f'{surname} {" ".join(phone_numbers[surname])}')
+    logger.number_logger(f"Добавляем контакт: {surname}")
+    return phone_numbers
 
 def delete_note():
-    pass
+    surname = input('введите имя для удаления ')
+    phone_numbers.pop(surname)
+    logger.number_logger(f"Удаляем контакт: {surname}")
+    return phone_numbers
