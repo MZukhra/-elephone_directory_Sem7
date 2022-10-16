ac3b79150e346b2b5c9f894b2d2dceafdf57ac19
«Zukhra» <sssmailll@mail.ru>
2022-10-16 22:06:04 +0300

    User_Interface

--- User_Interface.py
+++ User_Interface.py
@@ -1,19 +1,28 @@
-#Добавление удаление построчно
-#@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@
-#   author Yalushkin Alexey
-#@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@
+# Menu
+# @@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@
+#   author Zukhra
+# @@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@
 
 import input_output
-
-print('Добрый день')
-
-
-
+import add_delete
+import search
+import logger
 
 
-input_output.read_file()
-
-input_output.write_file()
-
+def menu():
+    z = input(
+        "Введите 1, если хотите найти контакт. \nВведите 2, если хотите отредактировать существующий. \nВведите 3, если хотите добавить новый контакт. \nВведите 4, если хотите удалить контакт. \n")
+    
+    match z:
+        case '1':
+            logger.number_logger("Выполняем поиск")
+            return search.search()
+        case '2':
+            logger.number_logger("Редактируем запись")
+            return add_delete.add_note()
+        case '3':
+            return add_delete.add_note()
+        case '4':
+            return add_delete.delete_note()
 
 
